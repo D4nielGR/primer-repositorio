8dd4655dc5b16e61e128445c4e76a369ff528fb8
D4nielGR <undanielguerrei@gmail.com>
2022-04-22 01:38:17 +0200

    Cambiado el ejercicio de 'mayor que' por un ejercicio de suma

--- primer_ficheroRepositorio.js
+++ primer_ficheroRepositorio.js
@@ -1,19 +1,9 @@
+// By Daniel Guerrero Reina
+// He cambiado todo el ejercicio por una simple función que sume ambas variables
 function Ej6(){
     let n1 = parseInt(document.getElementById("n1ej6").value);
     let n2 = parseInt(document.getElementById("n2ej6").value);
-    if(n1 > n2){
-        let suma = n1 + n2;
-        window.alert("La suma es: " + suma);
-        let resta = n1 - n2;
-        window.alert("La diferencia es: " + resta);
-    }
-    if(n2 > n1){
-        let multiplicacion = n1 * n2;
-        window.alert("El producto es: " + multiplicacion);
-        let division = n1 / n2;
-        window.alert("La division es: " + division);
-    }
-    if(n1 == n2){
-        window.alert(n1 + " y " + n2 + " son iguales");
-    }
+    
+    let suma = n1 + n2;
+    window.alert("La suma es: " + suma);
 }
