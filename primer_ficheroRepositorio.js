157f52bd0ce03b5ef8caee811ecddb7f9c22722a
D4anielGR <undanielguerrei@gmail.com>
2022-04-22 09:20:20 +0200

    Añadido la estructura a 'otroej2.html' y modificado la operación de suma por una resta

--- primer_ficheroRepositorio.js
+++ primer_ficheroRepositorio.js
@@ -1,9 +1,10 @@
 // By Daniel Guerrero Reina
 // He cambiado todo el ejercicio por una simple función que sume ambas variables
+// He modificado para que en vez de que sea una suma sea una resta
 function Ej6(){
     let n1 = parseInt(document.getElementById("n1ej6").value);
     let n2 = parseInt(document.getElementById("n2ej6").value);
     
-    let suma = n1 + n2;
-    window.alert("La suma es: " + suma);
+    let resta = n1 + n2;
+    window.alert("La resta es: " + resta);
 }
